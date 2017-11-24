c59d393b5c87ad14d99d9144db620d0345c0b601
Илья <iillyyaa1997@mail.ru>
2017-11-24 18:05:56 +0300

    bislogic

---
+++ WebSocket/BusinessLogic/Installer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace BusinessLogic
+{
+    public static class Installer
+    {
+        public static void AddBuisnessServices(this IServiceCollection container)
+        {
+            //container.AddScoped<IAdvertService, AdvertService>();
+        }
+    }
+}
