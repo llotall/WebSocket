b4344adabc94bf99f9824e404c2c8e71862c0eee
Илья <iillyyaa1997@mail.ru>
2017-11-24 18:30:28 +0300

    save

---
+++ WebSocket/Storage/SessionStorage.cs
@@ -0,0 +1,32 @@
+﻿using NHibernate;
+using Storage.Interfaces;
+
+namespace Storage
+{
+    public class SessionStorage : ISessionStorage
+    {
+        private readonly NHibernateConfigurator.ISessionFactory _sessionFactory;
+
+        private ISession _session;
+
+        public SessionStorage(NHibernateConfigurator.ISessionFactory sf)
+        {
+            _sessionFactory = sf;
+        }
+
+        public ISession Session
+        {
+            get
+            {
+                if (_session != null) return _session;
+
+                lock (this)
+                {
+                    _session = _session ?? _sessionFactory.Session;
+                }
+
+                return _session;
+            }
+        }
+    }
+}
