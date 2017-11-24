da6c104624ffa067d4e3da0b09c347538d65830a
Stanislav <llotall7@gmail.com>
2017-11-24 18:01:43 +0300

    Added ToketAuth

---
+++ WebSocket/WebSocket/Auth/TokenAuthOption.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+
+namespace Masterslavl.Auth
+{
+    public class TokenAuthOption
+    {
+        public static string Audience { get; } = "http://localhost:9955/";
+        public static string Issuer { get; } = "http://localhost:9955/";
+        public static RsaSecurityKey Key { get; } = new RsaSecurityKey(RSAKeyHelper.GenerateKey());
+        public static SigningCredentials SigningCredentials { get; } = new SigningCredentials(Key, SecurityAlgorithms.RsaSha256Signature);
+
+        public static TimeSpan ExpiresSpan { get; } = TimeSpan.FromMinutes(60);
+        public static string TokenType { get; } = "Bearer";
+    }
+}
+
---
+++ WebSocket/WebSocket/Controllers/TokenAuthController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Masterslavl.BusinessLogic.Interfaces.Base.CRUD;
+using Masterslavl.Models;
+using Masterslavl.Auth;
+using System;
+using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+using Masterslavl.Shared.JsonModels;
+using Masterslavl.Utils;
+using Masterslavl.Controllers;
+using Masterslavl.Shared.Entities;
+
+namespace Masterslavl.Controllers
+{
+    [Route("api/[controller]")]
+    public class TokenAuthController : BaseController
+    {
+        public IAdminService AdminService;
+        public ISessionAdminService SessionAdminService;
+
+        public TokenAuthController(
+            [FromServices] IAdminService adminService,
+            [FromServices] ISessionAdminService sessionAdminService)
+        {
+            AdminService = adminService;
+            SessionAdminService = sessionAdminService;
+        }
+
+        [HttpPost("get_auth_token")]
+        [AllowAnonymous]
+        public string GetAuthToken([FromBody]AdminJson user)
+        {
+            var existAdmin = AdminService.GetAll().FirstOrDefault(u => u.Login == user.Login && u.Password == user.Password);
+
+            var currentDate = DateTime.Now;
+
+            if (existAdmin != null)
+            {
+                var requestAt = currentDate;
+                var expiresIn = requestAt + TokenAuthOption.ExpiresSpan;
+                var token = GenerateToken(existAdmin, expiresIn);
+
+                return JsonConvert.SerializeObject(new RequestResult
+                {
+                    State = RequestState.Success,
+                    Data = new
+                    {
+                        requertAt = requestAt,
+                        expiresIn = TokenAuthOption.ExpiresSpan.TotalSeconds,
+                        tokeyType = TokenAuthOption.TokenType,
+                        accessToken = token
+                    }
+                });
+            }
+            else
+            {
+                return JsonConvert.SerializeObject(new RequestResult
+                {
+                    State = RequestState.Failed,
+                    Msg = "Username or password is invalid"
+                });
+            }
+        }
+
+        private string GenerateToken(Admin admin, DateTime expires)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            var claims = new List<Claim>
+            {
+                new Claim("Id", admin.ID.ToString()),
+                new Claim(ClaimsIdentity.DefaultNameClaimType, admin.Login),
+                new Claim(ClaimsIdentity.DefaultRoleClaimType, admin.Role)
+            };
+
+            ClaimsIdentity identity = new ClaimsIdentity(claims, "TokenAuth", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+
+            var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = TokenAuthOption.Issuer,
+                Audience = TokenAuthOption.Audience,
+                SigningCredentials = TokenAuthOption.SigningCredentials,
+                Subject = identity,
+                Expires = expires
+            });
+            return handler.WriteToken(securityToken);
+        }
+    }
+}
