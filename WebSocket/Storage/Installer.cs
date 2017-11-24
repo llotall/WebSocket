7ac1b42e3d978948e54675d806a3693d5b4d2c4e
Илья <iillyyaa1997@mail.ru>
2017-11-24 18:22:17 +0300

    save

---
+++ WebSocket/Storage/Installer.cs
@@ -0,0 +1,17 @@
+﻿using Storage.Interfaces;
+using Storage.Repsitories;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Storage
+{
+    public static class Installer
+    {
+        public static void AddNHibernate(this IServiceCollection container, IConfiguration config)
+        {
+            container.AddSingleton(typeof(NHibernateConfigurator.ISessionFactory), new NHibernateConfigurator.NHibernateConfiguration(config));
+            container.AddScoped<ISessionStorage, SessionStorage>();
+            container.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+        }
+    }
+}
---
+++ WebSocket/Storage/Interfaces/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using NHibernate;
+using Shared.Entities;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Storage.Interfaces
+{
+    public interface IRepository<T> where T : PersistentObject, new()
+    {
+        T Load(object nodeId);
+
+        T Get(long Id);
+
+        IQueryable<T> GetAll();
+
+        ISQLQuery ExecuteSqlQuery(string sqlQuery);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Update(T entity, long id);
+
+        void Remove(T entity);
+    }
+}
